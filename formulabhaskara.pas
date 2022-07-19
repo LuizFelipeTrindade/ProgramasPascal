24767df7e1b3a66fe3cbc04b27e4fde8f01cac0b
LuizFelipe <luizfelipe_14castro@hotmail.com>
2022-07-19 16:20:28 -0300

    Comentários sobre as funcionalidades

--- formulabhaskara.pas
+++ formulabhaskara.pas
@@ -1,9 +1,10 @@
 program bhaskara;
 var b, c , raizdiscriminante : real;
 begin
-read (b) ;
-read (c) ;
-raizdiscriminante:= sqrt (b * b - 4 * c) ;
-write ((b - raizdiscriminante )/ 2) ;
-write ((b + raizdiscriminante )/2) ;
+    read (b) ; //Leitura da variavel b
+    read (c) ; //Leitura da variavel c
+    raizdiscriminante:= sqrt (b * b - 4 * c) ;
+    write ((b - raizdiscriminante )/ 2) ; //impressão de valores
+    write ((b + raizdiscriminante )/2) ;
 end.
+
