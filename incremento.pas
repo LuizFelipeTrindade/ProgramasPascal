ca870ba1b6701b1de34932c69c576c6a6938a83b
LuizFelipe <luizfelipe_14castro@hotmail.com>
2022-07-19 17:33:53 -0300

    Programa de incremento com Pascal

---
+++ incremento.pas
@@ -0,0 +1,23 @@
+Program incremento ;
+
+var i: integer;     
+
+begin 
+   i := 0;
+   write(i);
+   i := i + 1;
+   write(i);
+end.
+
+
+Program ssm;
+
+var a, b : integer;
+
+begin 
+    read (a);
+    read (b);
+    write(a + b, a - b , a * b );
+end.
+
+
