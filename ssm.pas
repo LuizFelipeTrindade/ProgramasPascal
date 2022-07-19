f46b8a23d1c972f21e4fd9e816c0e2eb15417b9c
LuizFelipe <luizfelipe_14castro@hotmail.com>
2022-07-19 17:34:44 -0300

    Programa em Pascal para manipulação de números

---
+++ ssm.pas
@@ -0,0 +1,27 @@
+Program somaandleitura;
+
+var a, b : integer;
+
+var c : String ;
+
+begin
+
+c := 'Hi,Welcome, esse programa subtrai, multiplica e soma valores';
+writeln(c);
+
+write('Digite um valor: ');
+read (a);
+
+write('Digite novamente: ');
+read (b);
+
+write('Soma: ');
+writeln(a+b);
+
+write('Multi: ');
+writeln(a*b);
+
+write('Sub: ');
+writeln(a - b);
+
+end.
