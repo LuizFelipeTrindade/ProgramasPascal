d1bd284718ca488a81d7127120db98d9e6e19689
LuizFelipe <luizfelipe_14castro@hotmail.com>
2022-07-22 18:30:51 -0300

    Procedural Test in Pascal

---
+++ procedimento.pas
@@ -0,0 +1,15 @@
+program FunctionsInPascal;
+
+function //Nome //(Lista Parametros) : <Tipo>;
+
+//Cabeçalho da função
+
+//begin
+
+//end.
+
+//Lembrando que o a função é um subprograma
+//e tem que ser declarado no cabeçalho do programa principal, nunca dentro dele.
+
+
+
