444de32672daa123ec982fffb323a7d0a4206132
LuizFelipe <luizfelipe_14castro@hotmail.com>
2022-07-22 00:29:06 -0300

    Testando erros de sintaxe em Pascal

---
+++ testedeerros.pas
@@ -0,0 +1,15 @@
+program fomulaPorcento;	//Declaração do cabeçalho
+
+var a, b : integer; //Cabeçalho 
+
+begin
+  read (a);
+  read (b);
+
+writeln('primeiro numero:', a div 100);
+
+writeln ('A outra:', b div 100);
+//Programa principal
+
+end.
+//Termino do programa
