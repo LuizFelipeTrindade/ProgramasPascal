d1e2977f33a45d1f6a883097362694f541e8a512
LuizFelipe <luizfelipe_14castro@hotmail.com>
2022-07-22 20:40:02 -0300

    Creating a procedure for profiles in pascal

---
+++ TestedeProcedimentos.pas
@@ -0,0 +1,23 @@
+program outroProcedimento;
+
+procedure perfil (nome:string ; idade:integer ; peso:real) ;
+ begin
+    writeln ( 'Nome: ' , nome ) ;
+    writeln ( 'Idade: ' , idade ) ;
+    writeln ( 'Peso: ' , peso:0:1) ;
+ end ;
+var n : string ;i : integer ;p : real;
+begin
+    n := 'Blaise Pascal ';
+    i := 39;
+    p := 70.5;
+    perfil (n,i,p);
+end.
+
+//Programa princpal depois 
+//write('Your Name:');
+//    read (n);
+//   write('Your age: ');
+//    read (i);
+//   write('Your peso: ');
+//    read (p);
