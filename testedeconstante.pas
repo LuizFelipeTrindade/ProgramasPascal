d8abd634183c8a52f6c2d578aa172014a29f9145
LuizFelipe <luizfelipe_14castro@hotmail.com>
2022-07-22 18:30:03 -0300

    Testando o funcionamento das constantes em pascal

---
+++ testedeconstante.pas
@@ -0,0 +1,12 @@
+Program TestandoConstantes;
+
+const  
+ IDADE = integer; 
+ CPF = real;
+
+begin 
+    read (IDADE);
+    read (CPF);
+    //Elas não são lidas, pois o programa não reconhece ela como um variavel global
+end.
+
