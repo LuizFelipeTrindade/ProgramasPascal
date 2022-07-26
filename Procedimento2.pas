d98da206902b9093c3b763bb4cbb2e0a93450e34
LuizFelipe <luizfelipe_14castro@hotmail.com>
2022-07-26 17:53:56 -0300

    Criando mais um procedimento para impressão de cálculos inteiros e reais

---
+++ Procedimento2.pas
@@ -0,0 +1,12 @@
+Program Maisum_testede_procedimentos;
+
+procedure variosTipos( a , b : integer ; c : real ; d : integer ) ;
+begin
+writeln ( 'Inteiros : ' , a , ' e ' , b ) ;
+writeln ( 'Real: ' , c ) ;
+writeln ( 'Novamente inteiro: ' , d ) ;
+end ;
+
+begin
+  variosTipos
+end.
