005e0fda69237f6a37530b1cc6b63041f3dcff06
LuizFelipe <luizfelipe_14castro@hotmail.com>
2022-07-26 19:55:29 -0300

    Criando procedimento para duplicar números

---
+++ duplicnumber.pas
@@ -0,0 +1,22 @@
+Program duplic_number;
+
+//Código do livro
+
+procedure duplicador( n : integer);
+begin
+   n := 2 * n ;
+end;
+
+var number : integer;
+
+begin
+    number := 10;
+    duplicador(number);
+    writeln(number);
+end.
+
+
+             // Meu código//
+ //write ('Digite um numero inteiro:');
+ //read (n);
+ //writeln ('Number dupliced: ', n * 2);
