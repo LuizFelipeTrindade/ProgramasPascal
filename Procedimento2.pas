c96b7189caa8a31bf97f584e1506500985b15aa9
LuizFelipe <luizfelipe_14castro@hotmail.com>
2022-07-26 19:41:57 -0300

    Criando chamada para procedimentos no programa real

--- Procedimento2.pas
+++ Procedimento2.pas
@@ -1,12 +1,20 @@
 Program Maisum_testede_procedimentos;
 
-procedure variosTipos( a , b : integer ; c : real ; d : integer ) ;
+procedure variosTipos(a , b, d : integer; c : real );
 begin
 writeln ( 'Inteiros : ' , a , ' e ' , b ) ;
 writeln ( 'Real: ' , c ) ;
 writeln ( 'Novamente inteiro: ' , d ) ;
 end ;
 
+var a1 , b1, d1 : integer;
+var c1 : real;
+
 begin
-  variosTipos
+  write ('Number inteiros: ');
+  read (a1, b1, d1);
+
+  write('Real number: ');
+  read (c1);
+  variosTipos(a1,b1,d1,c1);
 end.
