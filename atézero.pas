00fef85adb2dbdc7d05baf56c135e5aee9d22747
LuizFelipe <luizfelipe_14castro@hotmail.com>
2022-07-28 10:03:13 -0300

    Contador infinito que chega até zero

---
+++ atézero.pas
@@ -0,0 +1,17 @@
+Program ate_zero;
+
+procedure conta_ate_zero( n : integer);
+begin
+
+if( n >= 0) then
+  begin
+    writeln(n);
+    conta_ate_zero(n - 1);
+  end;
+end;
+
+var number : integer;
+
+begin
+conta_ate_zero(number);
+end.
