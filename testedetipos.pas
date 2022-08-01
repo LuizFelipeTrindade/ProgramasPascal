8b57ae4d0af020b59d883a9a8cda7d06d6a7a367
LuizFelipe <luizfelipe_14castro@hotmail.com>
2022-08-01 17:02:09 -0300

    Criando novos tipos para variaveis

---
+++ testedetipos.pas
@@ -0,0 +1,17 @@
+Program Primitivos;
+
+Type Numeros : [1...10]; 
+Type vogais: [A...U];
+
+var number : Numberos;
+var vogal : vogais;
+
+begin
+
+write ('Digite um number: ');
+read (number);
+
+write ('Digite uma vogal: ');
+readln(vogais);
+
+end.
