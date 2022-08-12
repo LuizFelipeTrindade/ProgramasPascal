89d9c29b59b3e23e766e40b6484e49ceaa9a9825
LuizFelipe <luizfelipe_14castro@hotmail.com>
2022-08-12 10:01:14 -0300

    Programa de Condição(if, then, else) em pascal

---
+++ condicao.pas
@@ -0,0 +1,12 @@
+Program par;
+
+var a : integer;
+
+begin
+  write('Digite um numero inteiro positivo: ');
+  read(a);
+  if (a mod 2) = 0 then
+    write('O numero eh par')
+    else
+    write('O numero eh impar');
+end.
